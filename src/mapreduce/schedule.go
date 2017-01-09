af0686efdfc9e27a383b094048db209a2c6307cb
MK <slfs009@163.com>
2017-01-10 00:51:02 +0800

    update

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -4,15 +4,16 @@ import "fmt"
 import "sync/atomic"
 import "time"
 
-func addTaskToChannel(task_max int,c chan int){
+func addTaskToChannel(task_max int,c chan int,task_map *map[int]bool){
 	for i := 0; i < task_max; i++ {
 		c <- i
+		(*task_map)[i] = false
 	}
 }
 func (mr *Master)workerRun(
 				phase jobPhase,
 				task_c chan int,
-				task_finished *int64,
+				task_finished_c chan int,
 				worker_name string,
 				nios int){
 	var doTaskArgs DoTaskArgs
@@ -24,9 +25,10 @@ func (mr *Master)workerRun(
 		doTaskArgs.File = mr.files[task]
 		ok := call(worker_name, "Worker.DoTask", doTaskArgs, new(struct{}))	
 		if ok == true {
-			atomic.AddInt64(task_finished,1)
+			task_finished_c <- task
 		}else{
 			task_c <- task
+			break
 		}
 	}
 }
@@ -52,20 +54,28 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+	task_map := make(map[int]bool)
 	task_c := make(chan int)
-	go addTaskToChannel(ntasks,task_c)
+	finished_c := make(chan int)
+
+	go addTaskToChannel(ntasks,task_c,&task_map)
 	var task_finished int64
 	mr.Lock()
 	for _,worker := range mr.workers {
-		go mr.workerRun(phase,task_c,&task_finished,worker,nios)
+		go mr.workerRun(phase,task_c,finished_c,worker,nios)
 	}
 	mr.Unlock()
 	for atomic.LoadInt64(&task_finished) < int64(ntasks){
 		select {
 			case new_worker := <- mr.registerChannel:
-				go mr.workerRun(phase,task_c,&task_finished,new_worker,nios)
+				go mr.workerRun(phase,task_c,finished_c,new_worker,nios)
+			case finished_task := <- finished_c:
+				if task_map[finished_task] == false{
+					task_map[finished_task] = true
+					task_finished++
+				}
 			default:
-				time.Sleep(100 * time.Millisecond)	
+				time.Sleep(10 * time.Millisecond)	
 		}
 	}
 	close(task_c)
